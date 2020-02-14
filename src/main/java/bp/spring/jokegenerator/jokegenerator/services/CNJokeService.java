27f6b0d21fc4f62f61bf36657df5cb6b575772f7
piotr <PiotrBosak@yahoo.com>
2020-02-14 20:11:22 +0100

    added @Bean to use Java Configuration, we use when we use a class from external jar

--- src/main/java/bp/spring/jokegenerator/jokegenerator/services/CNJokeService.java
+++ src/main/java/bp/spring/jokegenerator/jokegenerator/services/CNJokeService.java
@@ -1,13 +1,20 @@
 package bp.spring.jokegenerator.jokegenerator.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service("goodJokes")
-@Profile({"chuck","default"})
+//@Profile({"chuck","default"})
 public class CNJokeService implements JokeService {
-    ChuckNorrisQuotes quotes = new ChuckNorrisQuotes();
+     private final ChuckNorrisQuotes quotes;
+
+
+    public CNJokeService(ChuckNorrisQuotes quotes) {
+        this.quotes = quotes;
+    }
+
     @Override
     public String getJoke() {
         return quotes.getRandomQuote();
